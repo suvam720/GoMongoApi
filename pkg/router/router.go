da535b4c8f8c691a0a36e7930a137320072c6f03
suvam720 <debnath.suvamhhh@gmail.com>
2022-04-25 19:30:23 +0530

    minor fix

--- pkg/router/router.go
+++ pkg/router/router.go
@@ -7,12 +7,12 @@ import (
 
 func Router() *mux.Router {
 	router := mux.NewRouter()
-	
+
 	router.HandleFunc("/api/task", controller.InsertOneTask).Methods("POST")
 	router.HandleFunc("/api/tasks", controller.GetAllTask).Methods("GET")
 	router.HandleFunc("/api/task/{id}", controller.MarkCompleted).Methods("PUT")
 	router.HandleFunc("/api/task/{id}", controller.DeleteOneTask).Methods("DELETE")
-	router.HandleFunc("/api/deletealltask", controller.DeleteAllTask).Methods("DELETE")
+	router.HandleFunc("/api/deletetasks", controller.DeleteAllTask).Methods("DELETE")
 
 	return router
 }
